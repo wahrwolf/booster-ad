12353068414edb2e1c5b00f5d96a7f9c0214fcef
philippklemmer <philipp@Philipps-MacBook-Pro.local>
2016-07-08 17:25:31 +0200

    frontend-js
    frontend

--- public/js/headerSlide.js
+++ public/js/headerSlide.js
@@ -16,64 +16,73 @@ $(function(){
 
 
     // Karten sind immer gleich groß
-    let form = $(".choose-profit");
-    var count = 0;
-    form.children().each(function(){
-        let test = $(this).outerWidth();
-        if(test > count){
-            count = test;
-        }
-    });
-    $('input').css({"width": count});
+    var form = $(".choose-profit");
+    // var count = 0;
+    // form.children().each(function(){
+    //     let test = $(this).outerWidth();
+    //     if(test > count){
+    //         count = test;
+    //     }
+    // });
+    // form.children().css({"width": count});
+    // $('input').css({"width": count});
 
     // Events
     //app
-    var app = $(".card-preview");
-    //app childs
-    var percentage = $("#percent");
-    var discount = $("#discount");
-    var children = form.children();
-    var newCardInfo = $(".new-card-info");
-    var moreInfos = $(".moreInfos");
-    var returnPage = $("#return");
-    // startbutton
-    var initBtn = $(".double-bounce");
+    function app(){
+        var cardPreview = $(".card-preview");
+        //app childs
 
-    form.children("input").each(function(){
-        $(this).on("click touch", function(){
-            children.hide();
-            newCardInfo.show();
-            newCardInfo.children().each(function(){
-                // wenn percent oder coins geklickt wurde wird beide
-                // male der gleich view aufgerufen
-                $(this).on("click touch", function(){
-                    newCardInfo.children().hide();
-                    let that = $(this).attr("name");
-                    if(that == "newCard"){
-                        // wenn new card geklickt wurde
-                            initBtn.on("click touch", function(){
-                                app.show();
-                            });
-                    }
-                    if(that == "moreInfos"){
-                        // wenn auf more infos geklickt wurde
-                        moreInfos.show();
-                        moreInfos.children().each(function(){
-                            $(this).on("click touch", function(){
-                                if($(this).attr('value')){
-                                    app.hide();
-                                }
-                            });
-
-                        });
-                    }
-                });
-            });
-
+        var percentage = $("#percent");
+        var discount = $("#discount");
+        var children = form.children();
+        var newCardInfo = $(".new-card-info");
+        var moreInfos = $(".moreInfos");
+        var returnPage = $("#return");
+        // startbutton
+        var initBtn = $("#eventTicker");
+        initBtn.on("click touch", function(){
+            cardPreview.show();
+            cardPreview.children().removeAttr('style');
         });
-    });
+        form.children("input").each(function(){
+            $(this).on("click touch", function(){
+                children.hide();
+                newCardInfo.show();
+                newCardInfo.children().each(function(){
+                    // wenn percent oder coins geklickt wurde wird beide
+                    // male der gleich view aufgerufen
+                    $(this).on("click touch", function(){
+                        newCardInfo.children().hide();
+                        let that = $(this).attr("name");
+                        if(that == "newCard"){
+                            // wenn new card geklickt wurde
+                                initBtn.on("click touch", function(){
+                                    cardPreview.show();
+                                });
+                        }
+                        if(that == "moreInfos"){
+                            // wenn auf more infos geklickt wurde
+                            moreInfos.show();
+                            moreInfos.children().each(function(){
+                                $(this).on("click touch", function(){
+                                    if($(this).attr('value')){
+                                        cardPreview.hide();
+                                        window.location.reload();
+                                    }
+                                });
 
+                            });
+                        }
+                    });
+                });
 
+            });
+        });
+
+    }
+
+    app();
 
 
 });
