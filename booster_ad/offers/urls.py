b406b523a7e558dfbb6f1021d53226ea4fefb348
Vincent Dahmen <4dahmen@informatik.uni-hamburg.de>
2016-07-08 18:08:40 +0200

    Profile: urls und view angepasst, Template bearbeitet

--- booster_ad/offers/urls.py
+++ booster_ad/offers/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = 'offers'
 urlpatterns = [
+	# ex: /offers/
 	url(r'^$', views.index, name='index'),
-
+	url(r'^user/(?P<userId>[0-9]+)/$', views.profile, name='profile'),
+	url(r'^user/(?P<userId>[0-9]+)/shop/$', views.shop, name='shop'),
 ]
--- booster_ad/offers/views.py
+++ booster_ad/offers/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
+from .models import User
 # Create your views here.
 
 def index(request):
 	return render(request, 'offers/index.html',{})
+
+def profile(request, userId):
+	user = get_object_or_404(User, pk=userId)
+	return render(request, 'offers/profile.html', {'user':user})
+
+def shop(request, userId):
+	user = get_object_or_404(User, pk=userId)
+	return HttpResponse("<p>No such bay</p>")
