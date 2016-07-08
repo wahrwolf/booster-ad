2433cd48352257f4f5bfb05613a5ef050fb07065
Vincent Dahmen <4dahmen@informatik.uni-hamburg.de>
2016-07-08 14:53:42 +0200

    cleanup

--- django/booster_ad/offers/admin.py
+++ django/booster_ad/offers/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import User, Offer, Brand, Category
+from .models import *
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Offer)
 admin.site.register(Brand)
 admin.site.register(Category)
+admin.site.register(Tag)
+
+admin.site.register(OfferTag)
+admin.site.register(BrandTag)
+admin.site.register(UserTag)
