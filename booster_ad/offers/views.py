72f2e4c27a1d7602d92d9c748f4237a9faff1834
Vincent Dahmen <4dahmen@informatik.uni-hamburg.de>
2016-07-09 11:31:28 +0200

    views: getOffer implemented

--- booster_ad/offers/views.py
+++ booster_ad/offers/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseForbidden, JsonResponse
 from django.conf import settings
+
+from random import randint
 
 ##Use for production
 #from .models import User, Offer
@@ -22,8 +24,12 @@ def shop(request, userId):
 	return HttpResponse("<p>No such bay</p>")
 
 def getOffer(request):
-	offers = Offer.objects.all()
-	return HttpResponseForbidden()
+	offers = Offer.objects.filter(status="open").values('description','coinValue', 'brand')
+	if offers.count() > 0:
+		offer = offers[randint(0,offers.count()-1)]
+		return JsonResponse(offer)
+	else:
+		return HttpResponseForbidden() #TODO find some better error ...
 
 def populate(request):
 	if settings.DEBUG:
